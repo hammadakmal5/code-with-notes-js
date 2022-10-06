e0ffd25020f952a015db83ef34504de59ddc779d
hammadakmal5 <hammadakmal5@gmail.com>
2022-10-06 14:54:46 +0500

    Week 02( Lesson 02 )

---
+++ Loops.js
@@ -0,0 +1,58 @@
+
+    ///// Loops: We use the to repeat a block of code
+/**
+ *  // How to create a loop
+ * // Big 3 Steps:
+ * 1. starting position of the loop(most commonly we start from zero)
+ * 
+ * 2. A condition(Ending position): Put the condition where your loop will stop
+ * 
+ * 3. i++ increment by one
+ */
+
+// There are three loops in JS
+// 1. for loop (use for loop where you exactly know how many times you want to repeat a code)
+
+// 2. while --- checks the condition first then execute the code only if the condition in true
+
+// 3. do while --- execute the code atleast once if the condition is fail
+
+
+
+// Step: 1
+
+
+// Step:2
+
+
+// Step:3
+
+
+// for(let i = 0; i < 3; i++) {
+//     // code to repeat
+//     console.log("kay");
+// }
+
+
+// while loop
+
+
+// let i = 0;
+
+
+// while (i < 3) {
+//     console.log("kwelani: " + i);
+//     i++;
+
+// }
+
+
+// do while
+
+let i = 10;
+
+do{
+        console.log("kwelani: " + i);
+        i++;
+    
+    }while (i < 3);
